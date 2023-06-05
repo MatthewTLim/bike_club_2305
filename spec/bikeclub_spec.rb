ac0a1d49490ad745b919b7c4f8dc5d74c3060016
Matthew Lim <matlim200@gmail.com>
2023-06-05 09:26:26 -0700

    Add #add_biker test

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -1,8 +1,14 @@
 require './lib/bikeclub'
+require './lib/ride'
+require './lib/biker'
 
 RSpec.describe BikeClub do
   before do
     @bikeclub = BikeClub.new("Ten Speeders")
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe "#exists" do
@@ -13,6 +19,14 @@ RSpec.describe BikeClub do
     it "has readable attibutes" do
       expect(@bikeclub.name).to eq("Ten Speeders")
       expect(@bikeclub.bikers).to eq([])
+    end
+  end
+
+  describe "#add_biker" do
+    it "can add bikers to the club" do
+      @bikeclub.add_biker(@biker)
+
+      expect(@bikeclub.bikers).to eq([@biker])
     end
   end
 end
