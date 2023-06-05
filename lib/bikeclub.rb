cf550ba688c4b1fb9de4a15d588738f15b863b56
Matthew Lim <matlim200@gmail.com>
2023-06-05 09:48:29 -0700

    Add #most_rides_logged method

--- lib/bikeclub.rb
+++ lib/bikeclub.rb
@@ -9,4 +9,15 @@ class BikeClub
   def add_biker(biker)
     @bikers << biker
   end
+
+  def most_rides_logged
+    most_rides = []
+    @bikers.each_with_index do |biker, index|
+      break if index == @bikers.length - 1
+      if biker.rides.values.count > @bikers[index + 1].rides.values.count
+        most_rides << biker
+      end
+    end
+    most_rides
+  end
 end
