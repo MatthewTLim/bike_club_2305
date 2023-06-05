7987d2a85da79a2c9cb43f83eb891da015090148
Matthew Lim <matlim200@gmail.com>
2023-06-05 08:21:25 -0700

    add #learn_terrain test

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe Biker do
       expect(@biker.acceptable_terrain).to eq([])
     end
   end
+
+  describe "#learn_terrain" do
+    it "can learn new terrains" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
 end
